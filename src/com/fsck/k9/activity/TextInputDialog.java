f5825f5b121aa977030aefeb443118dcb38dcdf5
Apoorv Khatreja <apoorvkhatreja@gmail.com>
2011-06-02 05:31:44 +0530

    Oops, forgot to add files for the last commit.

---
+++ src/com/fsck/k9/activity/TextInputDialog.java
@@ -0,0 +1,36 @@
+package com.fsck.k9.activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.view.View;
+
+public class TextInputDialog {
+
+
+
+    public static Dialog create(final Activity activity, final int dialogId, final int icon, final int title,
+                                final String message, final int confirmButton, final int cancelButton, final View dialogView) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setIcon(icon);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setView(dialogView);
+        builder.setPositiveButton(confirmButton,
+        new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                activity.dismissDialog(dialogId);
+            }
+        });
+        builder.setNegativeButton(cancelButton,
+        new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                activity.dismissDialog(dialogId);
+            }
+        });
+        return builder.create();
+    }
+}
